24fe623cf85da0f048ca8ebbefd8ab8aea3db37a
politick1101 <politick1101@gmail.com>
2023-11-20 02:25:11 +0300

    functions.py now have food icon preparation script

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from PIL import Image
 
@@ -13,15 +14,23 @@ def scale_image_up(image: Image.Image, scale: int) -> Image:
 
 
 if __name__ == '__main__':
+    # # preparing bigger versions of images
+    # source = 'resources_and_licenses/avatars/'
+    # result = 'images/avatars/'
+    #
+    # for filename in os.listdir(source):
+    #     image = Image.open(source + filename)
+    #
+    #     scaled_image = scale_image_up(image, 8)
+    #     scaled_image.save(result + filename)
 
-    # preparing bigger versions of images
+    # # crop out food icon
+    # width, height = 16, 16
+    # row_index, col_index = 1, 14
+    #
+    # box = (col_index * width, row_index * height,
+    #        (col_index + 1) * width, (row_index + 1) * height)
+    #
+    # Image.open('resources_and_licenses/Fruit+.png').crop(box).save('images/food_icon.png')
 
-    source = 'resources_and_licenses/avatars/'
-    result = 'images/avatars/'
-
-    for filename in os.listdir(source):
-
-        image = Image.open(source + filename)
-
-        scaled_image = scale_image_up(image, 8)
-        scaled_image.save(result + filename)
+    pass
