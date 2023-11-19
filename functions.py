f92fa7417a01579d5bbb982e08ada6e5cbd4fb28
politick1101 <politick1101@gmail.com>
2023-11-20 02:33:02 +0300

    functions.py todo added

--- functions.py
+++ functions.py
@@ -8,7 +8,9 @@ def scale_image_up(image: Image.Image, scale: int) -> Image:
     """returns scaled image"""
 
     new_size = image.width * scale, image.height * scale
-    new_image = image.resize(new_size)
+
+    # todo: resize function is blurry now, rewrite for pixel-by-pixel crisp scaling
+    new_image = image.resize(new_size, 5)
 
     return new_image
 
@@ -24,13 +26,13 @@ if __name__ == '__main__':
     #     scaled_image = scale_image_up(image, 8)
     #     scaled_image.save(result + filename)
 
-    # # crop out food icon
-    # width, height = 16, 16
-    # row_index, col_index = 1, 14
-    #
-    # box = (col_index * width, row_index * height,
-    #        (col_index + 1) * width, (row_index + 1) * height)
-    #
-    # Image.open('resources_and_licenses/Fruit+.png').crop(box).save('images/food_icon.png')
+    # crop out food icon
+    width, height = 16, 16
+    row_index, col_index = 1, 14
+
+    box = (col_index * width, row_index * height,
+           (col_index + 1) * width, (row_index + 1) * height)
+
+    scale_image_up(Image.open('resources_and_licenses/Fruit+.png').crop(box), 8).save('images/food_icon.png')
 
     pass
