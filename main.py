ddc584a5e9b417d8332d1410174b30023d99d94b
politick1101 <politick1101@gmail.com>
2023-11-20 16:51:19 +0300

    main was refactored a bit

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from PIL import Image, ImageDraw
 
 from fonts import *
 
-image = Image.open("images/avatars/avito_link.png")
 
-draw = ImageDraw.Draw(image)
+def draw_image_tag(image: Image.Image, tag: str) -> Image.Image:
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 10), tag, font=CS_MS_PIXEL_font, fill=(0, 0, 0))
+    return image
 
-draw.text((10, 10), "AvitoLink2015", font=CS_MS_PIXEL_font, fill=(0, 0, 0))
-
-image.show()
+avatar = Image.open("images/avatars/avito_link.png")
+tag = 'AvitoLink2014'
+status_image = draw_image_tag(avatar, tag)
+status_image.show()
