9df34a9091e92958635025bbbcc6ddd9d45534af
politick1101 <politick1101@gmail.com>
2023-11-20 15:06:09 +0300

    scripts to "prepare resources" are in prepare_resources.py now

--- functions.py
+++ functions.py
@@ -13,26 +13,3 @@ def scale_image_up(image: Image.Image, scale: int) -> Image:
     new_image = image.resize(new_size, 5)
 
     return new_image
-
-
-if __name__ == '__main__':
-    # # preparing bigger versions of images
-    # source = 'resources_and_licenses/avatars/'
-    # result = 'images/avatars/'
-    #
-    # for filename in os.listdir(source):
-    #     image = Image.open(source + filename)
-    #
-    #     scaled_image = scale_image_up(image, 8)
-    #     scaled_image.save(result + filename)
-
-    # crop out food icon
-    width, height = 16, 16
-    row_index, col_index = 1, 14
-
-    box = (col_index * width, row_index * height,
-           (col_index + 1) * width, (row_index + 1) * height)
-
-    scale_image_up(Image.open('resources_and_licenses/Fruit+.png').crop(box), 8).save('images/food_icon.png')
-
-    pass
---
+++ resources_and_licenses/prepare_resources.py
@@ -0,0 +1,24 @@
+import os
+
+from PIL import Image
+
+from functions import scale_image_up
+
+# # Preparing bigger versions of images.
+# source = 'resources_and_licenses/avatars/'
+# result = 'images/avatars/'
+#
+# for filename in os.listdir(source):
+#     image = Image.open(source + filename)
+#
+#     scaled_image = scale_image_up(image, 8)
+#     scaled_image.save(result + filename)
+
+# # crop out food icon
+# width, height = 16, 16
+# row_index, col_index = 1, 14
+#
+# box = (col_index * width, row_index * height,
+# (col_index + 1) * width, (row_index + 1) * height)
+#
+# scale_image_up(Image.open('resources_and_licenses/Fruit+.png').crop(box), 8).save('images/food_icon.png')
